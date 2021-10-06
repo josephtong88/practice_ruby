1d71ad0cc0591781c4ed89510ca01f3249f7ba0a
“Joseph <joseph@josephs-air.lan>
2021-10-05 18:23:24 -0700

    created class for store item

---
+++ store_item.rb
@@ -0,0 +1,61 @@
+# # shirt = { color: "black", price: 14.99, size: "large" }
+# # shirt2 = { color: "gray", price: 20, size: "medium" }
+# # shirt3 = { color: "white", price: 3.99, size: "small" }
+# # p shirt
+# # p shirt2
+# # p shirt3
+
+# # p "The #{shirt[:color]} costs $#{shirt[:price]} and comes in a size #{shirt[:size]}."
+
+class Item
+  def initialize(color, price, size)
+    @color = color
+    @price = price
+    @size = size
+  end
+
+  def color=(text)
+    @color = text
+  end
+
+  def color
+    @color
+  end
+
+  def price=(text)
+    @color = text
+  end
+
+  def price
+    @color
+  end
+
+  def size=(text)
+    @color = text
+  end
+
+  def size
+    @size
+  end
+
+  def info
+    p "The #{@color} shirt costs #{@price} and comes in size #{@size}."
+  end
+end
+
+item1 = Item.new("black", 14.99, "small")
+p item1
+item1.info
+
+# p "[C]reate [R]ead [U]pdate [D]elete [Q]uit"
+# response = gets.chomp.downcase
+# if response = "c"
+#   p "What color is the shirt?"
+#   color = gets.chomp
+#   p "How much does it cost?"
+#   price = gets.chomp.to_i
+#   p "What size is it?"
+#   size = gets.chomp
+#   item = Item.new(color, price, size)
+#   p item
+# end
